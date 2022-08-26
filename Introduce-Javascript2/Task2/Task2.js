a1db6a89c598ed458b9f8f3b992da40f96107d7f
maderisza <maderisza@gmail.com>
2022-08-26 13:36:21 +0700

    update task 2

--- Introduce-Javascript2/Task2/Task2.js
+++ Introduce-Javascript2/Task2/Task2.js
@@ -15,30 +15,30 @@
 // });
 // console.log(patients.slice(0,3));
 function masukan(str,limit,callback){
-    var patients = [
+    let patients = [
         "Abigail", "Alexandra", "Alison",
         "Amanda", "Angela", "Bella",
         "Carol", "Caroline", "Carolyn",
         "Deirdre", "Diana", "Elizabeth",
         "Ella", "Faith", "Olivia", "Penelope"
     ]
-    callback(str,limit,patients)
+    callback(str,limit,patients) // untuk memanggil fungsi search name (penghubung func masukan)
 }
-function searchname(input, size, patients){
-    var patients = [
-            "Abigail", "Alexandra", "Alison",
-            "Amanda", "Angela", "Bella",
-            "Carol", "Caroline", "Carolyn",
-            "Deirdre", "Diana", "Elizabeth",
-            "Ella", "Faith", "Olivia", "Penelope"
-        ]
+function searchname(input, size, data){
+    // var patients = [
+    //         "Abigail", "Alexandra", "Alison",
+    //         "Amanda", "Angela", "Bella",
+    //         "Carol", "Caroline", "Carolyn",
+    //         "Deirdre", "Diana", "Elizabeth",
+    //         "Ella", "Faith", "Olivia", "Penelope"
+    //     ]
         // var input = "ab";
-        var re = new RegExp(input+'.+$', 'i'); // regexp untuk: digunakan untuk melakukan fungsi  pencarian ganti pencocokan pola pada teks 
-        patients = patients.filter(function(e, i, a){  //+$ memperbolehkan huruf besa, huruf kecil,angka
+        let re = new RegExp(input+'.+$', 'i'); // regexp untuk: digunakan untuk melakukan fungsi  pencarian ganti pencocokan pola pada teks 
+        data = data.filter(function(e, i, a){  //+$ memperbolehkan huruf besa, huruf kecil,angka
             return e.search(re) != -1; // menuliskan kembali ke dalam string                  // menentukan kecocokan huruf besar huruf kecil
         });
         // hasil(limit)
-        console.log(patients.slice(0,size));
+        console.log(data.slice(0,size));
 }
 // function hasil(size){
 //     console.log(patients.slice(0,size));
